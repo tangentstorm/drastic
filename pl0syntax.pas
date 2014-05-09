417c1c96fab0d1a931b4079325328eacc51f5ae2
tangentstorm <michal.wallace@gmail.com>
2014-05-09 17:29:52 -0500

    generate spaces automatically in varshow

--- pl0syntax.pas
+++ pl0syntax.pas
@@ -47,25 +47,20 @@ procedure VarShow(v : TVar);
     if VarIsStr(v) then cwrite(v)
     else if VarIsArray(v) then
       if VarIsStr(v[0]) or VarIsArray(v[0]) then
+	//  TODO: ignore spaces if last item was kNB (to fix seq)
         for item in TVars(v) do VarShow(item)
       else try case TKind(v[0]) of
         kNB   : cwrite([ '|K', TStr(v[1])]);
         kHBox : for item in behead( TVars( v )) do varshow(item);
         kLit : cwrite([ '|B', TStr( v[1]) ]);
         kSub : cwrite([ '|m', TStr( v[1]) ]);
-        kOpt : varshow(implode(' ', A([ '|r(', behead( TVars(v)), '|r)?' ])));
-        kRep : varshow(implode(' ', A([ '|r(', behead( TVars(v)), '|r)+' ])));
-        kOrp : varshow(implode(' ', A([ '|r(', behead( TVars(v)), '|r)*' ])));
-        kAlt : varshow(implode(' ', A([ '|r(', implode(' |r|| ', v[1]),
-				        ' |r)' ])));
-        kSeq : if length(TVars(v[1])) > 0 then begin
-                 varshow(TVars(v[1])[0]);
-                 cwrite('|>');
-                 for item in behead(TVars(v[1])) do varshow(item);
-                 cwrite('|<');
-               end;
-        kDef : VarShow(A(['|R@|y ', v[1], '|_|R:',
-                           implode('|_|r||', v[2]), nl, nl ]));
+        kOpt : varshow(A([ '|r( ', implode(' ', v[1]), ' |r)?' ]));
+        kRep : varshow(A([ '|r( ', implode(' ', v[1]), ' |r)+' ]));
+        kOrp : varshow(A([ '|r( ', implode(' ', v[1]), ' |r)*' ]));
+        kAlt : varshow(A([ '|r( ', implode(' |r|| ', v[1]), ' |r)' ]));
+        kSeq : varshow(A([ '|>', implode(' ', v[1]), '|<' ]));
+        kDef : VarShow(A(['|R@|y ', v[1], '|_|R: ',
+                           implode('|_|r|| ' , v[2]), nl, nl ]));
         otherwise
           cwrite('|!r|y'); write('<', TKind(v[0]), '>'); cwrite('|w|!k');
       end except on e:EVariantError do for item in tvars(v) do varshow(v) end
@@ -82,145 +77,54 @@ begin
     nb('from Algorithms and Data Structures by Niklaus Wirth.'), nl,
 
     def('program', [
-      seq([ ' ', sub('block'), ' ', lit('.') ]) ]),
+      seq([ sub('block'), lit('.') ]) ]),
 
     def('block', [ seq([
-      ' ',
+
       opt([ lit('const'),
-            ' ',
-            rep([ sub('ident'),
-                  ' ',
-                  lit('='),
-                  ' ',
-                  sub('number'),
-                  ' ',
-                  '|r/',
-                  ' ',
-                  lit(',') ]),
-            ' ',
+            rep([ sub('ident'), lit('='), sub('number'), '|r/', lit(',') ]),
             lit(';') ]),
       nl,
-      opt([ lit('var'),
-            ' ',
-            rep([ sub('ident'),
-                  ' ',
-                  '|r/',
-                  ' ',
-                  lit(',') ]),
-            ' ',
-            lit(';') ]),
+      opt([ lit('var'), rep([ sub('ident'), '|r/', lit(',') ]), lit(';') ]),
       nl,
-      orp([ lit('procedure'),
-            ' ',
-            sub('ident'),
-            ' ',
-            lit(';'),
-            ' ',
-            sub('block'),
-            ' ',
-            lit(';') ]),
+      orp([ lit('procedure'), sub('ident'), lit(';'), sub('block'), lit(';') ]),
       nl,
       sub('statement') ]) ]),
 
     def('statement', [
-      seq([ ' ',
-            sub('ident'),
-            ' ',
-            lit(':='),
-            ' ',
-            sub('expression') ]),
-      seq([ ' ',
-            lit('call'),
-            ' ',
-            sub('ident') ]),
-      seq([ ' ',
-            lit('begin'),
-            ' ',
-            sub('statement'),
-            ' ',
-            orp([ lit(';'),
-                  ' ',
-                  sub('statement')]),
-            ' ',
+      seq([ sub('ident'), lit(':='), sub('expression') ]),
+      seq([ lit('call'), sub('ident') ]),
+      seq([ lit('begin'),
+	      sub('statement'),
+	      orp([ lit(';'), sub('statement')]),
             lit('end') ]),
-      seq([ ' ',
-            lit('if'),
-            ' ',
-            sub('condition'),
-            ' ',
-            lit('then'),
-            ' ',
-            sub('statement') ]),
-      seq([ ' ',
-            lit('while'),
-            ' ',
-            sub('condition'),
-            ' ',
-            lit('do'),
-            ' ',
-            sub('statement') ]),
-      nb(' empty statement') ]),
+      seq([ lit('if'), sub('condition'), lit('then'), sub('statement') ]),
+      seq([ lit('while'), sub('condition'), lit('do'), sub('statement') ]),
+      nb('empty statement') ]),
 
     def( 'condition', [
-      seq([ ' ',
-            lit('odd'),
-            ' ',
-            sub('expression') ]),
-      seq([ ' ',
-            sub('expression'),
-            ' ',
-         // grp([        //  TODO
-            alt([ lit('='),
-                  lit('<'),
-                  lit('≠'),
-                  lit('>'),
-                  lit('≤'),
-                  lit('≥') ]),
-            ' ',
+      seq([ lit('odd'), sub('expression') ]),
+      seq([ sub('expression'),
+            alt([ lit('='), lit('≠'),
+                  lit('<'), lit('>'),
+                  lit('≤'), lit('≥') ]),
             sub('expression') ]) ]),
 
     def('expression', [
-      seq([ '|r (',
-                 ' ',
-                 lit('+'),
-                 ' ',
-                 '|r||',
-                 lit('-'),
-                 ' ', '|r)',
-           ' ',
-           sub('term'),
-           ' ',
-           orp([ '|r(',
-                     ' ',
-                     lit('+'),
-                     ' ',
-                     '|r||',
-                     ' ',
-                     lit('-'),
-                     ' ', '|r)',
-                 ' ',
-                 sub('term') ]) ]) ]),
+      seq([ alt([ lit('+'), lit('-') ]),
+            sub('term'),
+            orp([ alt([ lit('+'), lit('-') ]),
+                  sub('term') ]) ]) ]),
 
     def('term', [
-      seq([ ' ',
-            sub('factor'),
-            ' ',
-            orp([ '(',
-                      ' ',
-                      lit('×'),
-                      ' ',
-                      '|r||',
-                      ' ',
-                      lit('÷'),
-                      ' ', '|r)',
-                 ' ',
-                 sub('factor') ]) ]) ]),
+      seq([ sub('factor'),
+            orp([ alt([ lit('×'), lit('÷') ]),
+                  sub('factor') ]) ]) ]),
 
     def('factor', [
-      seq([ ' ', sub('ident') ]),
-      seq([ ' ', sub('number') ]),
-      seq([ ' ', lit('('), ' ', sub('expression'), ' ', lit(')') ]) ]),
+      sub('ident'),
+      sub('number'),
+      seq([ lit('('), sub('expression'), lit(')') ]) ]),
 
-    '|w'
-  ]))
+    '|w' ]))
 end.
