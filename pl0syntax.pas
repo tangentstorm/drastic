cfd12ef661bd60cc0aab782840c30864f98b6342
tangentstorm <michal.wallace@gmail.com>
2014-05-09 02:33:29 -0500

    added an indenting combinator for seq

--- pl0syntax.pas
+++ pl0syntax.pas
@@ -1,7 +1,7 @@
 { drastic: direct representation of abstract syntax trees }
 {$mode delphiunicode}{$i xpc.inc}
 program drastic;
-uses xpc,kvm,cw,variants;
+uses xpc,kvm,cw,variants,sysutils;
 
 {-- constructor for array of variants -------------------------}
 
@@ -13,16 +13,33 @@ function L(vars : array of TVar) : TVars; inline;
   begin result := g<TVar>.fromOpenArray(vars);
   end;
 
+{-- helpers ----------------------------------------------------}
+
+function drop(n : word; vars: tvars) : TVars; inline;
+  var i : integer;
+  begin
+    if length(vars) > n then begin
+      setlength(result, length(vars)-n);
+      for i := n to length(vars) -1 do result[i-n] := vars[i];;
+    end
+   else setlength(result,0)
+  end;
+
 
 {-- data constructors -----------------------------------------}
 
-type TKind = ( kNB, kNL, kRule );
+type TKind = ( kNB, kNL, kSeq, kRule );
 
 function nb( comment : TStr ) : TVar;
   begin result := L([kNB, comment])
   end;
 
 const nl = kNL;
+
+function seq(parts : array of TVar) : TVar;
+  begin result := L([kSeq, L(parts)])
+  end;
+
 
 function rule(iden : TStr; alts : array of TVar) : TVar;
   begin result := L([kRule, iden, L(alts)])
@@ -36,34 +53,39 @@ procedure VarShow(v : TVar);
   begin
     if VarIsStr(v) then cwrite(v)
     else if VarIsArray(v) then
-      if VarIsStr(v[0])
-        then for item in TVars(v) do VarShow(item)
-        else case TKind(v[0]) of
-	  kNB : cwrite(['|K', TStr(v[1])]);
-	  kRule : VarShow(L(['|R@|y ' + v[1], nl, v[2], nl, nl ]));
-          otherwise
-	end
-    else if TKind(v) = kNL then newline
+      if VarIsStr(v[0]) then
+        for item in TVars(v) do VarShow(item)
+      else if VarIsArray(v[0]) then varshow(v[0])
+      else try case TKind(v[0]) of
+        kNB  : cwrite(['|K', TStr(v[1])]);
+        kSeq : if length(TVars(v[1])) > 1 then begin
+                 varshow(TVars(v[1])[0]);
+                 cwrite('|>');
+                 for item in drop(1, TVars(v[1])) do varshow(item);
+                 cwrite('|<');
+               end;
+        kRule : VarShow(L(['|R@|y ' + v[1], nl, v[2], nl, nl ]));
+        otherwise
+      end except on e:EVariantError do for item in tvars(v) do varshow(v) end
+    else if TKind(v) = kNL then cwrite('|_') // newline but with indentation
   end;
 
 {-- main : shows pretty grammar for PL/0 in color --------------}
 
-var v : TVar;
 begin
   clrscr;
   VarShow(L([
     '|wPL/0 syntax', nl,
-    nb('from Algorithms and Data Structures by Niklaus Wirth.'),
-    nl,
+    nb('from Algorithms and Data Structures by Niklaus Wirth.'), nl,
 
     rule('program', [
     '|R:|m block |B.' ]),
 
-    rule('block', [
+    rule('block', [ seq([
     '|R:|r (|B const |r( |mident |B= |mnumber |r/ |B, |r)+ |B; |r)?', nl,
-    '|r  (|B var |r( |mident |r/ |B, |r)+ |B; |r)?',  nl,
-    '|r  (|B procedure |mident |B; |mblock |B; |r)*',  nl,
-    '|r  |mstatement' ]),
+    '|r(|B var |r( |mident |r/ |B, |r)+ |B; |r)?',  nl,
+    '|r(|B procedure |mident |B; |mblock |B; |r)*',  nl,
+    '|r|mstatement' ]) ]),
 
     rule('statement', [
     '|R:|m ident |B:= |mexpression', nl,
