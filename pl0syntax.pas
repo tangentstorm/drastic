50858bfe375bf1e83e18cb681f374ce8e9f77ae9
tangentstorm <michal.wallace@gmail.com>
2014-05-09 15:54:07 -0500

    moved tvar stuff to uvar.pas (in xpl repo)

--- pl0syntax.pas
+++ pl0syntax.pas
@@ -1,61 +1,20 @@
 { drastic: direct representation of abstract syntax trees }
 {$mode delphiunicode}{$i xpc.inc}
 program drastic;
-uses xpc,kvm,cw,variants,sysutils;
-
-{-- constructor for array of variants -------------------------}
-
-type
-  TVar = variant;
-  TVars = array of TVar;
-
-function A(vars : array of TVar) : TVars; inline;
-  begin result := g<TVar>.fromOpenArray(vars);
-  end;
-
-{-- helpers ----------------------------------------------------}
-
-function drop(n : word; vars: tvars) : TVars; inline;
-  var i : integer;
-  begin
-    if length(vars) > n then begin
-      setlength(result, length(vars)-n);
-      for i := n to length(vars) -1 do result[i-n] := vars[i];;
-    end
-   else setlength(result,0)
-  end;
-
-function implode(glue : tvar; vars: tvars) : TVars; inline;
-  var i : integer;
-  begin
-    if length(vars) <= 1 then result := vars
-    else begin
-      setlength(result, length(vars)*2-1);
-      for i := 0 to length(vars)-2 do begin
-        result[i*2]:=vars[i]; result[i*2+1]:=glue
-      end;
-      result[2*(length(vars)-1)] := vars[length(vars)-1]
-    end
-  end;
-
+uses xpc, kvm, cw, variants, uvar, sysutils;
 
 {-- data constructors -----------------------------------------}
 
-type TKind = ( kNB, kNL, kSeq, kRule, kLit, kSub, kOpt, kRep, kOrp, kHBox );
-
-function nb( comment : TStr ) : TVar;
-  begin result := A([kNB, comment])
-  end;
+type TKind = ( kNB, kNL, kSeq, kAlt, kRule, kLit, kSub, kOpt, kRep, kOrp, kHBox );
 
 const nl = kNL;
 
-function seq(parts : array of TVar) : TVar;
-  begin result := A([kSeq, A(parts)])
-  end;
-
+function nb (s : TStr) : TVar; begin result := A([kNB,  s]) end;
 function lit(s : TStr) : TVar; begin result := A([kLIT, s]) end;
 function sub(s : TStr) : TVar; begin result := A([kSub, s]) end;
 
+function seq(vars : array of TVar):TVar; begin result:=A([kSeq, A(vars)]) end;
+function alt(vars : array of TVar):TVar; begin result:=A([kAlt, A(vars)]) end;
 function opt(vars : array of TVar):TVar; begin result:=A([kOpt, A(vars)]) end;
 function rep(vars : array of TVar):TVar; begin result:=A([kRep, A(vars)]) end;
 function orp(vars : array of TVar):TVar; begin result:=A([kOrp, A(vars)]) end;
@@ -68,7 +27,7 @@ function hbox(vars : array of TVar) : TVar;
   begin result := A([kHBox, A(vars)])
   end;
 
-
+
 {-- recursive show for variants -------------------------------}
 
 procedure VarShow(v : TVar);
@@ -80,12 +39,15 @@ procedure VarShow(v : TVar);
         for item in TVars(v) do VarShow(item)
       else try case TKind(v[0]) of
         kNB   : cwrite([ '|K', TStr(v[1])]);
-        kHBox : for item in drop(1, TVars(v)) do varshow(item);
-        kLit : cwrite([ '|B', TStr(v[1]) ]);
-        kSub : cwrite([ '|m', TStr(v[1]) ]);
-        kOpt : varshow(implode(' ', A([ '|r(', drop(1, TVars(v)), '|r)?' ])));
-        kRep : varshow(implode(' ', A([ '|r(', drop(1, TVars(v)), '|r)+' ])));
-        kOrp : varshow(implode(' ', A([ '|r(', drop(1, TVars(v)), '|r)*' ])));
+        kHBox : for item in behead( TVars( v )) do varshow(item);
+        kLit : cwrite([ '|B', TStr( v[1]) ]);
+        kSub : cwrite([ '|m', TStr( v[1]) ]);
+        kOpt : varshow(implode(' ', A([ '|r(', behead( TVars(v)), '|r)?' ])));
+        kRep : varshow(implode(' ', A([ '|r(', behead( TVars(v)), '|r)+' ])));
+        kOrp : varshow(implode(' ', A([ '|r(', behead( TVars(v)), '|r)*' ])));
+        kAlt : varshow(implode(' ', A([ '|r( ',
+				        implode( ' |r|| ', behead( TVars( v ))),
+				        ' |r)' ])));
         kSeq : if length(TVars(v[1])) > 0 then begin
                  varshow(TVars(v[1])[0]);
                  cwrite('|>');
@@ -94,11 +56,12 @@ procedure VarShow(v : TVar);
                end;
         kRule : VarShow(A(['|R@|y ', v[1], '|_|R:',
                            implode('|_|r||', v[2]), nl, nl ]));
-        otherwise for item in tvars(v) do varshow(v)
+        otherwise
+          cwrite('|!r|y'); write('<', TKind(v[0]), '>'); cwrite('|w|!k');
       end except on e:EVariantError do for item in tvars(v) do varshow(v) end
     else if TKind(v) = kNL then cwrite('|_') // newline but with indentation
   end;
-
+
 {-- main : shows pretty grammar for PL/0 in color --------------}
 
 begin
@@ -196,30 +159,12 @@ begin
             sub('expression'),
             ' ',
          // grp([        //  TODO
-            '|r(',
-                  ' ',
-                  lit('='),
-                  ' ',
-                  '|r||',
-                  ' ',
+            alt([ lit('='),
                   lit('<'),
-                  ' ',
-                  '|r||',
-                  ' ',
                   lit('≠'),
-                  ' ',
-                  '|r||',
-                  ' ',
                   lit('>'),
-                  ' ',
-                  '|r||',
-                  ' ',
                   lit('≤'),
-                  ' ',
-                  '|r||',
-                  ' ',
-                  lit('≥'),
-                  ' ', '|r)',
+                  lit('≥') ]),
             ' ',
             sub('expression') ]) ]),
 
