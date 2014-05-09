a21791a8518f87df9e5cd0854fcf5b62003cf7bd
tangentstorm <michal.wallace@gmail.com>
2014-05-09 13:01:52 -0500

    factored out [opt,rep,orp] and started on [lit,sub]

--- pl0syntax.pas
+++ pl0syntax.pas
@@ -32,7 +32,7 @@ function implode(glue : tvar; vars: tvars) : TVars; inline;
     else begin
       setlength(result, length(vars)*2-1);
       for i := 0 to length(vars)-2 do begin
-	result[i*2]:=vars[i]; result[i*2+1]:=glue
+        result[i*2]:=vars[i]; result[i*2+1]:=glue
       end;
       result[2*(length(vars)-1)] := vars[length(vars)-1]
     end
@@ -41,7 +41,7 @@ function implode(glue : tvar; vars: tvars) : TVars; inline;
 
 {-- data constructors -----------------------------------------}
 
-type TKind = ( kNB, kNL, kSeq, kRule, kHBox );
+type TKind = ( kNB, kNL, kSeq, kRule, kLit, kSub, kOpt, kRep, kOrp, kHBox );
 
 function nb( comment : TStr ) : TVar;
   begin result := L([kNB, comment])
@@ -53,6 +53,12 @@ function seq(parts : array of TVar) : TVar;
   begin result := L([kSeq, L(parts)])
   end;
 
+function lit(s : TStr) : TVar; begin result := L([kLIT, s]) end;
+function sub(s : TStr) : TVar; begin result := L([kSub, s]) end;
+
+function opt(vars : array of TVar):TVar; begin result:=L([kOpt, L(vars)]) end;
+function rep(vars : array of TVar):TVar; begin result:=L([kRep, L(vars)]) end;
+function orp(vars : array of TVar):TVar; begin result:=L([kOrp, L(vars)]) end;
 
 function rule(iden : TStr; alts : array of TVar) : TVar;
   begin result := L([kRule, iden, L(alts)])
@@ -70,21 +76,25 @@ procedure VarShow(v : TVar);
   begin
     if VarIsStr(v) then cwrite(v)
     else if VarIsArray(v) then
-      if VarIsStr(v[0]) then
+      if VarIsStr(v[0]) or VarIsArray(v[0]) then
         for item in TVars(v) do VarShow(item)
-      else if VarIsArray(v[0]) then varshow(v[0])
       else try case TKind(v[0]) of
         kNB   : cwrite(['|K', TStr(v[1])]);
-	kHBox : for item in drop(1, TVars(v)) do varshow(item);
-        kSeq : if length(TVars(v[1])) > 1 then begin
+        kHBox : for item in drop(1, TVars(v)) do varshow(item);
+        kLit : cwrite(['|M', TStr(v[1])]);
+        kSub : cwrite(['|C', TStr(v[1])]);
+        kOpt : varshow(implode(' ', L(['|r(', drop(1, TVars(v)), '|r)?' ])));
+        kRep : varshow(implode(' ', L(['|r(', drop(1, TVars(v)), '|r)+' ])));
+        kOrp : varshow(implode(' ', L(['|r(', drop(1, TVars(v)), '|r)*' ])));
+        kSeq : if length(TVars(v[1])) > 0 then begin
                  varshow(TVars(v[1])[0]);
                  cwrite('|>');
                  for item in drop(1, TVars(v[1])) do varshow(item);
                  cwrite('|<');
                end;
-        kRule : VarShow(L(['|R@|y ' + v[1] + '|_|R:',
-			   implode('|_|r||', v[2]), nl, nl ]));
-        otherwise
+        kRule : VarShow(L(['|R@|y ', v[1], '|_|R:',
+                           implode('|_|r||', v[2]), nl, nl ]));
+        otherwise for item in tvars(v) do varshow(v)
       end except on e:EVariantError do for item in tvars(v) do varshow(v) end
     else if TKind(v) = kNL then cwrite('|_') // newline but with indentation
   end;
@@ -98,38 +108,47 @@ begin
     nb('from Algorithms and Data Structures by Niklaus Wirth.'), nl,
 
     rule('program', [
-    '|m block |B.' ]),
+      seq([ ' ', sub('block'), ' ', lit('.') ]) ]),
 
     rule('block', [ seq([
-    '|r (|B const |r( |mident |B= |mnumber |r/ |B, |r)+ |B; |r)?', nl,
-    '|r(|B var |r( |mident |r/ |B, |r)+ |B; |r)?', nl,
-    '|r(|B procedure |mident |B; |mblock |B; |r)*', nl,
-    '|r|mstatement' ]) ]),
+      ' ',
+      opt(['|Bconst |r( |mident |B= |mnumber |r/ |B, |r)+ |B;']), nl,
+      opt(['|Bvar |r( |mident |r/ |B, |r)+ |B;']), nl,
+      orp(['|Bprocedure |mident |B; |mblock |B;']), nl,
+      sub('statement') ]) ]),
 
     rule('statement', [
-    '|m ident |B:= |mexpression',
-    '|B call |mident',
-    '|B begin |mstatement |r( |B; |mstatement |r)* |Bend',
-    '|B if |mcondition |Bthen |mstatement',
-    '|B while |mcondition |Bdo |mstatement',
-    nb(' empty statement') ]),
+      seq(['|m ident |B:= |mexpression']),
+      seq(['|B call', ' ', '|mident' ]),
+      seq(['|B begin |mstatement',
+	   ' ',
+	   orp(['|B; |mstatement']),
+	   ' ',
+	   '|Bend']),
+      seq(['|B if |mcondition |Bthen |mstatement' ]),
+      seq(['|B while |mcondition |Bdo |mstatement' ]),
+      nb(' empty statement') ]),
 
     rule('condition', [
-    '|B odd |mexpression',
-    '|m expression '
+      seq(['|B odd |mexpression']),
+      seq(['|m expression '
         + '|r( |B= |r|||B < |r|||B ≠ |r|||B > |r|||B ≤ |r|||B ≥ |r)'
-        + '|m expression' ]),
+        + '|m expression' ]) ]),
 
     rule('expression', [
-    '|r ( |B+ |r|||B - |r) |mterm |r(( |B+ |r|||B - |r) |mterm|r )*' ]),
+      seq(['|r ( |B+ |r|||B - |r) |mterm',
+	   ' ',
+	   orp(['( |B+ |r|||B - |r) |mterm']) ]) ]),
 
     rule('term', [
-    '|m factor |r((|B × |r|||B ÷ |r) |mfactor|r )*' ]),
+      seq(['|m factor',
+	   ' ',
+	   orp(['(|B × |r|||B ÷ |r) |mfactor' ]) ]) ]),
 
     rule('factor', [
-    '|m ident',
-    '|m number',
-    '|B (|m expression |B)' ]),
+      seq([ ' ', sub('ident') ]),
+      seq([ ' ', sub('number') ]),
+      seq([ ' ', lit('('), sub('expression'), lit(')') ]) ]),
 
     '|w'
   ]))
